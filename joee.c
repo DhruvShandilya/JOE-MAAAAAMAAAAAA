91a8467c9c1bf1fd5c609a6fd0e23637cf059212
DhruvShandilya <120823147+DhruvShandilya@users.noreply.github.com>
2022-12-18 00:49:11 +0530

    Create joee.c

---
+++ joee.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+int a,b,c,age;
+ /* printf("enter the value of a:");
+  scanf("%d"'&a);
+  printf("enter the value of b:");
+  scanf("%d"'&b);
+  printf("enter the value of c:");
+  scanf("%d"'&c); */
+        
+  printf("enter the value of age:");
+  scanf("%d"'&age);
+  
+        
+  if(age>18){
+         printf("YOU CAN DRIVE");
+  }      
+  else{
+         printtf("500 FINE");
+  }
+        
+
+return 0;
+}
