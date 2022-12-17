19f338633a80a62d4c762bffeeaf26de1fbe165a
DhruvShandilya <120823147+DhruvShandilya@users.noreply.github.com>
2022-12-18 01:09:32 +0530

    Update joee.c

--- joee.c
+++ joee.c
@@ -2,21 +2,21 @@
 int main(){
 int a,b,c,age;
  /* printf("enter the value of a:");
-  scanf("%d"'&a);
+  scanf("%d",&a);
   printf("enter the value of b:");
-  scanf("%d"'&b);
+  scanf("%d",&b);
   printf("enter the value of c:");
-  scanf("%d"'&c); */
+  scanf("%d",&c); */
         
   printf("enter the value of age:");
-  scanf("%d"'&age);
+  scanf("%d",&age);
   
         
   if(age>18){
          printf("YOU CAN DRIVE");
   }      
   else{
-         printtf("500 FINE");
+         printf("500 FINE");
   }
         
 
